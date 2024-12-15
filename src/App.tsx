da16f07a7d44b96bfc56e542ec649c5aa3dc8dfe
crab85193 <crab85193@gmail.com>
2024-12-15 21:27:38 +0900

    feat: Headerを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from "react";
-import logo from "./assets/images/logo.svg";
-import "./assets/styles/App.css";
+import Header from "./components/Header";
+import "./assets/styles/reset.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./../assets/styles/Header.css";
+
+function Header() {
+  return (
+    <header className="site-header">
+      <div className="wrapper site-header__wrapper">
+        <a href="" className="brand">
+          都道府県別の総人口推移グラフ
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
