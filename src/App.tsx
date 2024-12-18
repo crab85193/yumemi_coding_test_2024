0c0b513e9552adb5bece40e0c5c817f1f941c00c
crab85193 <crab85193@gmail.com>
2024-12-18 20:34:33 +0900

    feat: レイアウト修正とグラフ描画機能の統合準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import PopulationGraphPage from "./pages/PopulationGraphPage";
 import "./assets/styles/reset.css";
 
 function App() {
   return (
     <div className="layout">
       <Header />
+      <PopulationGraphPage />
     </div>
   );
 }
